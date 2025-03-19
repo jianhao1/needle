c7c3e335298a1325e3f50c6f6bf15720d48e36ee
jianhao1 <99834505@qq.com>
2025-03-20 02:55:27 +0800

    initializers

--- python/needle/init/init_initializers.py
+++ python/needle/init/init_initializers.py
@@ -2,27 +2,37 @@ import math
 from .init_basic import *
 
 
-def xavier_uniform(fan_in, fan_out, gain=1.0, **kwargs):
-    ### BEGIN YOUR SOLUTION
-    raise NotImplementedError()
-    ### END YOUR SOLUTION
+def xavier_uniform(fan_in, fan_out, shape=None, gain=1.0, **kwargs):
+    a = gain * math.sqrt(6/(fan_in+fan_out))
+    if shape is None:
+      shape = (fan_in, fan_out)
+    return rand(*shape, low=-a, high=a, **kwargs)
 
 
-def xavier_normal(fan_in, fan_out, gain=1.0, **kwargs):
-    ### BEGIN YOUR SOLUTION
-    raise NotImplementedError()
-    ### END YOUR SOLUTION
+def xavier_normal(fan_in, fan_out, shape=None, gain=1.0, **kwargs):
+    std = gain * math.sqrt(2/(fan_in+fan_out))
+    if shape is None:
+      shape = (fan_in, fan_out)
+    return randn(*shape, std=std, **kwargs)
 
 
 
 def kaiming_uniform(fan_in, fan_out, shape=None, nonlinearity="relu", **kwargs):
     assert nonlinearity == "relu", "Only relu supported currently"
-    ### BEGIN YOUR SOLUTION
-    raise NotImplementedError()
-    ### END YOUR SOLUTION
+    gain = 1
+    if nonlinearity == "relu":
+      gain = math.sqrt(2)
+    bound = gain * math.sqrt(3/fan_in)
+    if shape is None:
+      shape = (fan_in, fan_out)
+    return rand(*shape, low=-bound, high=bound, **kwargs)
 
-def kaiming_normal(fan_in, fan_out, nonlinearity="relu", **kwargs):
+def kaiming_normal(fan_in, fan_out, shape=None, nonlinearity="relu", **kwargs):
     assert nonlinearity == "relu", "Only relu supported currently"
-    ### BEGIN YOUR SOLUTION
-    raise NotImplementedError()
-    ### END YOUR SOLUTION
+    gain = 1
+    if nonlinearity == "relu":
+      gain = math.sqrt(2)
+    std = gain / math.sqrt(fan_in)
+    if shape is None:
+      shape = (fan_in, fan_out)
+    return randn(*shape, std=std, **kwargs)
